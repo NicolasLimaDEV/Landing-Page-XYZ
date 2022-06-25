4e5fdfae27aaa20a3c1bcd41a5132e79287aebfc
Nicolas Lima <91435296+NicolasLimaDEV@users.noreply.github.com>
2022-06-25 19:30:31 -0300

    Arquivos LP

---
+++ script.js
@@ -0,0 +1,5 @@
+let btnMobile = document.querySelector('.btn-mobile');
+btnMobile.addEventListener('click', function menuShow(){
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('active')
+})
